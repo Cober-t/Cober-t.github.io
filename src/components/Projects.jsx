0704a2f1d6403389ba3cc8e26cea8c82aa8b1970
Cober-t <jorgeouscober@gmail.com>
2024-09-19 19:23:18 +0200

    [Add] GameEngine Descriptions

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,7 @@ const projects = [
             num: '0.0',
             title: "Core",
             description: "A la hora de comenzar un proyecto como el de un motor de videojuegos \
-            existen una serie de características fundamentales que beneficia la creción de cualquier juego.\
+            existen una serie de características fundamentales que beneficia la creción de cualquier videojuego.\
             A continuación explico el desarrollo de dichas características a través de proyectos terminados.",
             stack: [{name: "C++"}],
             image: "./unity/parallaxImage.jpg",
@@ -47,17 +47,97 @@ const projects = [
         {
             num: '0.3',
             title: "Editor",
-            description: "Un editor visual para el motor supone una drástica mejora al permitir \
-            la creación de herramientas que aceleran y facilitan el proceso de desarrollo de videjuegos.",
+            description: "Un editor visual para el motor supone una drástica mejora, permitiendo \
+            la creación de herramientas que aceleran y facilitan el de desarrollo de videjuegos.",
             stack: [{name: "ImGui"}, {name: "Immediate Mode"}],
             image: "./myGameEngine/parallaxImage.png",
             github: "",
         },
+        {
+            num: '0.4',
+            title: "API para los gráficos",
+            description: "Al igual que sucede con la ventana, es importante crear una capa que nos permita \
+            mostrar gráficos en ella. Cada sistema operativo gestiona los graficos de diferente forma, pero el motor \
+            debe estar preparado para que no sea dificil añadir más capacidades gráficas en un futuro. \
+            El motor usa OpenGL porque funciona en un gran número de plataformas diferentes.",
+            stack: [{name: "OpenGL"}],
+            image: "./myGameEngine/parallaxImage.png",
+            github: "",
+        },
+        {
+            num: '0.5',
+            title: "Escena de juego y Sistema de Entidades",
+            description: "A la hora de diseñar la manera en la que el motor va a gestionar los elementos del juego \
+            se ha optado por un sistema de entidades. Todo elemento en el juego (entidad) va a tener una lista \
+            de componentes: posición, físicas, texturas, etc. que a su vez forman sistemas propios. \
+            Una escena del juego recorre estos sistermas y a su vez todas las entidades que pertencen a estos, \
+            haciendo que el procesamiento sea muy rápido al evitar repeticiones innecesarias y busquedas en memoria muy largas",
+            stack: [{name: "entt"}, {name: "Gestión de memoria"}, {name: "Estructuras de datos"}],
+            image: "./myGameEngine/parallaxImage.png",
+            github: "",
+        },
+        {
+            num: '0.6',
+            title: "Scripting nativo",
+            description: "Gracias a una correcta configuración del proyecto y a las librerías dinámicas, con este sistema \
+            podremos programar la lógica del videojuego sin tener que recompilar todo el motor, pudiendo hacer \
+            cambios y desarrollar el juego mientras el motor está en ejecución. Aunque no es una característica imprescindible \
+            si que facilita enormenente el desarrollo, y es sistema que podemos encontrar en todos los motores populares",
+            stack: [{name: "C++"}, {name: "Dll"}, {name: "Scripting Nativo"}],
+            image: "./myGameEngine/parallaxImage.png",
+            github: "",
+        },
+        {
+            num: '0.7',
+            title: "Launcher para el motor y proyectos",
+            description: "Al igual que otros populares motores de videojuegos, un launcher visual facilita la interacción \
+            con el motor, sus posibles versiones y la creación y edición de proyectos en desarrollo",
+            stack: [{name: "Qt"}, {name: "Python"}],
+            image: "./myGameEngine/parallaxImage.png",
+            github: "",
+        },
+        {
+            num: '1',
+            title: "Pong",
+            description: "Para poder desarrollar este primer proyecto era necesario implementar nuevas funcionalidades en el motor \
+            que usaran como base para los siguientes, estas son: simulación de físicas en 2D para la colisión de \
+            la pelota con las palas de los jugadores, un sistema de audio que permita reproducir diferentes sonidos cuando \
+            ciertos eventos ocurran y un sistema de cámaras que permita modificar facilmente aspectos como la resolución, el zoom, \
+            la posición y ángulo de la cámara o la relación de aspecto",
+            stack: [{name: "box2d"}, {name: "miniaudio"}, {name: "Cámara virtual"}],
+            image: "./myGameEngine/Pong.png",
+            github: "",
+        },
+        {
+            num: '2',
+            title: "Flappy Bird",
+            description: "En este proyecto se implementa la carga de texturas, con las que a su vez \
+            podremos dibujar texto en pantalla. La serialización de datos, que permite guardar y cargar escenas, \
+            imprescindible para la persistencia de datos en un videojuego y para evitar empezar desde una escena vacía \
+            en el desarrollo de un proyecto. Otra sustancial mejora que nos permite la serialización de datos es \
+            la creción de 'prefabs', entidades completas que podremos guardar \
+            editar, duplicar, siempre atendiendo al uso de memoria y las estructuras de datos.",
+            stack: [{name: "Texturas y texto"}, {name: "Serialización de datos"}, {name: "Prefabs"}],
+            image: "./myGameEngine/FlappyBird.png",
+            github: "",
+        },
+        {
+            num: '3',
+            title: "Breakout",
+            description: "Este proyecto hace un uso intensivo de las texturas, y para gestionarlas de manera correcta existen técnicas \
+            muy utilizadas como la del 'atlas de textura': una única textura agrupará todas las que se usen en el juego, \
+            disponiendo de ellas a través de índices o coordenadas. Todas se dibujan a la vez, ahorrando procesamiento \
+            y memoria gráfica, lo que nos permite desarrollar herramientas como el sistema de partículas, \
+            con el que podremos editar los atributos de las miles de texturas que se van a dibujar",
+            stack: [{name: "Atlas de texturas"}, {name: "Sistema de partículas"}, {name: "Flujo jugable"}],
+            image: "./myGameEngine/Breakout.png",
+            github: "",
+        },
     ],
     [
         {
             num: '01',
-            title: "project 1",
+            title: "Generador de terreno aleatorio",
             description: "asdassaddd asda dasda adas asd asd",
             stack: [{name: "React"}, {name: "C++"}, {name: "Particle System"}],
             image: "./unity/parallaxImage.jpg",
@@ -65,24 +145,7 @@ const projects = [
         },
         {
             num: '02',
-            category: 'frontend',
-            title: "project 2",
-            description: "asdassaddd asda dasda adas asd asd",
-            stack: [{name: "React"}, {name: "C++"}, {name: "Particle System"}],
-            image: "./myGameEngine/parallaxImage.png",
-            github: "",
-        },
-        {
-            num: '03',
-            title: "project 3",
-            description: "asdassaddd asda dasda adas asd asd",
-            stack: [{name: "React"}, {name: "C++"}, {name: "Particle System"}],
-            image: "./unity/parallaxImage.jpg",
-            github: "",
-        },
-        {
-            num: '04',
-            title: "project 4",
+            title: "Sistema de diálogo",
             description: "asdassaddd asda dasda adas asd asd",
             stack: [{name: "React"}, {name: "C++"}, {name: "Particle System"}],
             image: "./myGameEngine/parallaxImage.png",
@@ -92,35 +155,34 @@ const projects = [
     [
         {
             num: '01',
-            title: "project 1",
+            title: "Validador de modelos y animaciones para importar a Unreal Engine",
             description: "asdassaddd asda dasda adas asd asd",
-            stack: [{name: "React"}, {name: "C++"}, {name: "Particle System"}],
-            image: "./unity/parallaxImage.jpg",
+            stack: [{name: "Qt"}, {name: "Pyside"}, {name: "Maya"}, {name: "Blender"}],
+            image: "./myGameEngine/parallaxImage.png",
             github: "",
         },
         {
             num: '02',
-            title: "project 2",
+            title: "Servidor para validar nomenclaturas de archivos",
             description: "asdassaddd asda dasda adas asd asd",
-            stack: [{name: "React"}, {name: "C++"}, {name: "Particle System"}],
+            stack: [{name: "Redes"}, {name: "Expresiones regulares (regex)"}],
             image: "./myGameEngine/parallaxImage.png",
             github: "",
         },
         {
             num: '03',
-            category: 'frontend',
-            title: "project 3",
+            title: "Gestión de Apps a través de Telegram",
             description: "asdassaddd asda dasda adas asd asd",
-            stack: [{name: "React"}, {name: "C++"}, {name: "Particle System"}],
+            stack: [{name: "RaspberryPi"}, {name: "Todoist"}, {name: "Notion"}, {name: "Obsidian"}],
             image: "./unity/parallaxImage.jpg",
             github: "",
         },
         {
             num: '04',
-            title: "project 4",
+            title: "Launcher para proyectos del motor de videojuegos",
             description: "asdassaddd asda dasda adas asd asd",
-            stack: [{name: "React"}, {name: "C++"}, {name: "Particle System"}],
-            image: "./myGameEngine/parallaxImage.png",
+            stack: [{name: "Qt"}, {name: "UI"}],
+            image: "./unity/parallaxImage.jpg",
             github: "",
         },
     ],
@@ -144,22 +206,11 @@ const Projects = () => {
     const handleSlideChange = (swiper) => {
 
         const projectSize = projects[currentProject].length
-        console.log(swiper.activeIndex)
 
         if (swiper.activeIndex === 0)
-        {
-            // if (slideIndex === 3)
-            //     setProgress(100 / projectSize)
-            // else
-                setProgress(100 / projectSize * swiper.activeIndex + 100 / projectSize)
-        }
+            setProgress(100 / projectSize * swiper.activeIndex + 100 / projectSize)
         else if (swiper.activeIndex === projectSize - 1)
-        {
-            // if (slideIndex === 0)
-            //     setProgress(100)
-            // else
-                setProgress(100 / projectSize * swiper.activeIndex + 100 / projectSize)
-        }
+            setProgress(100 / projectSize * swiper.activeIndex + 100 / projectSize)
         else
             setProgress(100 / projectSize * swiper.activeIndex + 100 / projectSize)
 
@@ -170,6 +221,7 @@ const Projects = () => {
     useEffect(function() {
 
         setProject(projects[currentProject][0])
+        setProgress(100/projects[currentProject].length)
 
     }, [currentProject])
 
@@ -284,7 +336,7 @@ const Projects = () => {
                             animate={{width: `${progress}%`}}
                             onAnimationStart={()=> {setLastProgressValue(progress)}}
                             transition={{duration:0.75, ease:"easeInOut"}}
-                            className={`bg-accent h-[5px] mb-8`}>
+                            className={`bg-accent h-[7px] mb-8`}>
                             </motion.div>
                         </div>
                     </div>
