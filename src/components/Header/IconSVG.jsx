f56860520006ddf47a1d56859cd95b62220e1e84
Cober-t <jorgeouscober@gmail.com>
2024-09-27 11:24:59 +0200

    [Add] Publish

--- src/components/Header/IconSVG.jsx
+++
@@ -1,8 +0,0 @@
-
-
-const IconSVG = (props) => {
-    return <img src={props.path} fill="currentColor" stroke="currentColor"
-    className={props.class} onClick={props.onClick}/>
-}
-
-export default IconSVG
